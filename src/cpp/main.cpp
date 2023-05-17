3d6cce921891630a2d1a9fdef489308a10790677
Maximilien Nowak <pro.mnowak@gmail.com>
2023-05-17 10:57:49 +1000

    include OpenMP

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,7 +1,19 @@
 #include "default.h"
 
+#include <omp.h>
+
 int main()
 {
-    std::cout << "hello world !" << std::endl;
+    int i;
+    int threadID = 0;
+    #pragma omp parallel for private(i, threadID)
+    for(i = 0; i < 16; i++ )
+    {
+        threadID = omp_get_thread_num();
+        #pragma omp critical
+        {
+            std::cout << "Hello from thread " << threadID << " with number " << i << std::endl;
+        }
+    }
     return 0;
 }
