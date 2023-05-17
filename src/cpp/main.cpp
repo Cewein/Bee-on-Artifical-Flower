45729933de1ffa8bae290a5de2fcd79cf611be29
Maximilien Nowak <pro.mnowak@gmail.com>
2023-05-17 14:39:45 +1000

    multithread dir reading

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,19 +1,43 @@
 #include "default.h"
 
 #include <omp.h>
+#include <filesystem>
+#include <string>
+
+#include <dirent.h> 
+#include <stdio.h> 
 
 int main()
 {
-    int i;
+    int i = 0;
     int threadID = 0;
-    #pragma omp parallel for private(i, threadID)
-    for(i = 0; i < 16; i++ )
-    {
-        threadID = omp_get_thread_num();
-        #pragma omp critical
+    std::string fileName = "";
+    std::string path = "/home/cewein/Documents/content-0.15-0.20-0.25/Bee-on-Artifical-Flower/yolov7/runs/detect/exp/labels";
+
+    //#pragma omp parallel for private(i, threadID)
+    
+    DIR *d;
+    d = opendir(path.c_str());
+    struct dirent * dir;
+    
+
+    if (d) {
+
+        #pragma omp parallel private(threadID) shared(dir, d)
+        while ((dir = readdir(d)) != NULL)
         {
-            std::cout << "Hello from thread " << threadID << " with number " << i << std::endl;
+            threadID = omp_get_thread_num();
+
+            #pragma omp critical
+            if(dir != NULL)
+            {
+                std::cout << "Thread " << threadID << "\t" << dir->d_name << std::endl;
+                dir = readdir(d);
+            }      
         }
+        closedir(d);
     }
+
+
     return 0;
 }
